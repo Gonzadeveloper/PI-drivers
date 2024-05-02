61d745df023b814a61710db2180edef1f88ce6a3
Gonzadevolper <gonza.cay@gmail.com>
2024-05-02 00:42:56 -0300

    Deploy 3

--- server/src/db.js
+++ server/src/db.js
@@ -3,7 +3,8 @@ const { Sequelize } = require("sequelize");
 const drivers = require('./models/Driver')
 const teams = require('./models/Teams')
 
-
+const fs = require('fs');
+const path = require('path');
 const {
   DB
 } = process.env;
@@ -11,6 +12,22 @@ const {
 const sequelize = new Sequelize(DB, {
   logging: false, 
 });
+const basename = path.basename(__filename);
+
+const modelDefiners = [];
+
+fs.readdirSync(path.join(__dirname, '/models'))
+  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+  });
+
+
+modelDefiners.forEach(model => model(sequelize));
+
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+sequelize.models = Object.fromEntries(capsEntries);
 
 const { Driver, Teams } = sequelize.models;
 
