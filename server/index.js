874323c509e7ea12d403ca7149359ccf5021fb3f
Gonzadevolper <gonza.cay@gmail.com>
2024-07-24 01:47:21 -0300

    con

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ const server = require("./src/server");
 
 const express = require('express');
 const app = express();
-const port =  3000;
+const PORT=  3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
