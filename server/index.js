a1c61dc3a5fc5f501925a52a5975a25bacb1805d
Gonzadevolper <gonza.cay@gmail.com>
2024-07-24 01:26:49 -0300

    5

--- server/index.js
+++ server/index.js
@@ -4,18 +4,12 @@
 // const { loadTeamsFromAPI } = require('./src/controllers/getAllTeams.js');
 // const server = require("./src/server");
 
-const express = require('express');
-const app = express()
-const PORT =  process.env.PORT || 3000
+// const express = require('express');
+// const app = express()
+// const PORT =  process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-// Middleware para procesar JSON y URL-encoded bodies
+// //Middleware para procesar JSON y URL-encoded bodies
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -26,3 +20,25 @@ app.listen(PORT, () => {
 //   console.log(`Server listening on port ${PORT}`);
 // })
 // }).catch(error => console.error(error))
+
+
+require("dotenv").config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const { conn } = require('./src/db.js');
+const { loadTeamsFromAPI } = require('./src/controllers/getAllTeams.js');
+
+const app = express();
+const PORT = process.env.PORT || 3000; // Asegúrate de tener un valor por defecto en caso de que PORT no esté definido
+
+// Middleware para procesar JSON y URL-encoded bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+conn.sync({ force: false }).then(() => {
+  loadTeamsFromAPI();
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+}).catch(error => console.error(error));
+
