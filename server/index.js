cf41473e30f1dfa2b501ab6f1c2de6d59aedac8f
Gonzadevolper <gonza.cay@gmail.com>
2024-07-24 01:05:45 -0300

    3

--- server/index.js
+++ server/index.js
@@ -1,21 +1,25 @@
-require ("dotenv").config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const { conn } = require('./src/db.js');
-const { loadTeamsFromAPI } = require('./src/controllers/getAllTeams.js');
-const server = require("./src/server");
+// require ("dotenv").config();
+// const bodyParser = require('body-parser');
+// const { conn } = require('./src/db.js');
+// const { loadTeamsFromAPI } = require('./src/controllers/getAllTeams.js');
+// const server = require("./src/server");
 
+const express = require('express');
 const app = express()
 const PORT =  process.env.PORT 
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 // Middleware para procesar JSON y URL-encoded bodies
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 
-conn.sync({ force: false }).then(() => {
-  loadTeamsFromAPI();
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-})
-}).catch(error => console.error(error))
+// conn.sync({ force: false }).then(() => {
+//   loadTeamsFromAPI();
+// server.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// })
+// }).catch(error => console.error(error))
