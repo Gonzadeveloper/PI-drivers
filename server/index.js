4cf1257b685e7795ee90fbb697c3e96f64370fab
Gonzadevolper <gonza.cay@gmail.com>
2024-07-24 02:00:15 -0300

    s

--- server/index.js
+++ server/index.js
@@ -1,44 +1,26 @@
-// require ("dotenv").config();
-// const bodyParser = require('body-parser');
-// const { conn } = require('./src/db.js');
-// const { loadTeamsFromAPI } = require('./src/controllers/getAllTeams.js');
-// const server = require("./src/server");
-
-// const express = require('express');
-// const app = express()
-// const PORT =  process.env.PORT || 3000
-
-
-// //Middleware para procesar JSON y URL-encoded bodies
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-
-// conn.sync({ force: false }).then(() => {
-//   loadTeamsFromAPI();
-// server.listen(PORT, () => {
-//   console.log(`Server listening on port ${PORT}`);
-// })
-// }).catch(error => console.error(error))
-
-
-//codigo viejo
-require ("dotenv").config();
+require("dotenv").config();
+const express = require('express');
 const bodyParser = require('body-parser');
 const { conn } = require('./src/db.js');
-//const { loadTeamsFromAPI } = require('./src/controllers/getAllTeams.js');
-const server = require("./src/server");
+const { loadTeamsFromAPI } = require('./src/controllers/getAllTeams.js');
 
-
-const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // Asegúrate de tener un valor por defecto
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// Middleware para procesar JSON y URL-encoded bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Sincroniza la base de datos y luego carga los equipos
+conn.sync({ force: false }).then(() => {
+  // Cargar equipos después de sincronizar la base de datos
+  loadTeamsFromAPI().catch(error => {
+    console.error('Error loading teams:', error);
+  });
+  
+  // Inicia el servidor
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+}).catch(error => console.error('Database connection error:', error));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
